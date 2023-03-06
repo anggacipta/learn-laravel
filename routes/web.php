486ab90c60a7185e458e11c5e047e21be0d25d25
anggacipta <anggasusanto12@gmail.com>
2023-03-06 21:31:08 +0700

    add static file and route parameter

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,29 @@ Route::get('/hello-again', function (){
 Route::get('/hello-world', function (){
     return view('hello.helloworld', ['name' => 'Angga']);
 });
+
+
+Route::get('/products/{id}', function ($productId){
+   return "Product $productId";
+});
+
+Route::get('/products/{id}/item/{item}', function ($productId, $itemId){
+    return "Product $productId, Item $itemId";
+});
+
+Route::get('/categories/{id}', function ($categoryId){
+    return "Category : " . $categoryId;
+})->where('id', '[0-9]+');
+
+Route::get('/users/{id?}', function ($userId = '404'){
+    return "User $userId";
+});
+
+Route::get('/conflict/angga', function ($name){
+    return "Conflict Angga Cipta Pranata";
+});
+
+Route::get('/conflict/{name}', function ($name){
+    return "Conflict $name";
+});
+
--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -32,4 +32,46 @@ class RoutingTest extends TestCase
         $this->get('/oraonok')->
         assertSeeText('Not found page');
     }
+
+    public function testProductRouteParameter()
+    {
+        $this->get('/products/1')
+            ->assertSeeText('Product 1');
+
+        $this->get('/products/2')
+            ->assertSeeText('Product 2');
+
+        $this->get('/products/1/item/xxx')
+            ->assertSeeText('Product 1, Item xxx');
+
+        $this->get('/products/2/item/xxx')
+            ->assertSeeText('Product 2, Item yyy');
+    }
+
+    public function testRouteParameterRegex()
+    {
+        $this->get('categories/1234')
+        ->assertSeeText('Category : 1234');
+
+        $this->get('categories/salah')
+            ->assertSeeText('Not found page');
+    }
+
+    public function testRouteParameterOptional()
+    {
+        $this->get('/users/angga')
+            ->assertSeeText('User angga');
+
+        $this->get('/users/')
+            ->assertSeeText('User 404');
+    }
+
+    public function testRouteConflict()
+    {
+        $this->get('/conflict/angga')
+            ->assertSeeText('Conflict angga');
+
+        $this->get('/conflict/angga')
+            ->assertSeeText('Conflict Angga Cipta Pranata');
+    }
 }

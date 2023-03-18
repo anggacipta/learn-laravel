05d4cfec2063b496eb1992bce8cffac08e094efb
anggacipta <anggasusanto12@gmail.com>
2023-03-18 22:42:46 +0700

    testing http exception and maintenance mode

--- routes/web.php
+++ routes/web.php
@@ -163,3 +163,13 @@ Route::get('/error/manual', function (){
 Route::get('/error/validation', function (){
    throw new \App\Exceptions\ValidationException('Validation Error');
 });
+
+Route::get('/abort/400', function (){
+    abort(400, 'Validation Error, sorry');
+});
+Route::get('/abort/401', function (){
+    abort(401);
+});
+Route::get('/abort/500', function (){
+    abort(500);
+});
